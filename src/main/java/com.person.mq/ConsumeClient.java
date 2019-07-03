db8a2839aa748cf539adf6065c7e87bb1682a11b
wangbiao <wangbiao@ybm100.com>
2019-07-03 19:53:05 +0800

    初始化项目

---
+++ src/main/java/com.person.mq/ConsumeClient.java
@@ -0,0 +1,12 @@
+package com.person.mq;
+
+/**
+ * @author : wangbiao
+ * @version V1.0
+ * @Project: myMQ-demo
+ * @Package com.person.mq
+ * @Description: TODO
+ * @date Date : 2019年07月03日 19:51
+ */
+public class ConsumeClient {
+}
