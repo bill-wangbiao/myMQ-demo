07582a0bba174cecb57cfc693bb061b9d0ca4363
wangbiao <wangbiao@ybm100.com>
2019-07-03 19:55:36 +0800

    初始化项目

---
+++ src/main/java/com.person.mq/activeMq/QueueConsumer.java
@@ -0,0 +1,12 @@
+package com.person.mq.activeMq;
+
+/**
+ * @author : wangbiao
+ * @version V1.0
+ * @Project: myMQ-demo
+ * @Package com.person.mq.activeMq
+ * @Description: TODO
+ * @date Date : 2019年07月03日 19:54
+ */
+public class QueueConsumer {
+}
